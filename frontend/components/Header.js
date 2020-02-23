fc9a683c69db0a8f3e08577b55143f68fe53af71
vihong <probamaths@gmail.com>
2020-02-23 17:56:43 +0100

    :lipstick: Add styling to Header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,21 +1,60 @@
 import Link from 'next/link';
 import Nav from './Nav';
+import styled from 'styled-components';
 
 function Header(props) {
 	return (
-		<div>
-			<div className="bar">
-				<Link href="/">
-					<a>PokeXchange</a>
-				</Link>
-			</div>
+		<HeaderStyled>
+			<Link href="/">
+				<LogoStyled>
+					<a>PokéXchange</a>
+				</LogoStyled>
+			</Link>
 			<Nav />
-			<div className="sub-bar">
-				<p>Search</p>
-			</div>
-			<div>Cart</div>
-		</div>
+			<input placeholder="Rechercher un Pokemon" />
+			<label>&#128722;</label>
+		</HeaderStyled>
 	);
 }
+
+const HeaderStyled = styled.header`
+	font-family: Nunito, sans-serif;
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	padding: 15px 5%;
+	background: #112c68;
+	input {
+		padding: 8px 25px;
+		margin: 0 15px;
+		border-radius: 5px;
+		border: none;
+		width: 150px;
+		&[type=text] {
+			background-color: green;
+			color: red;
+			padding: 0;
+			font-size: 5rem;
+		}
+	}
+	label {
+		cursor: pointer;
+		font-size: 20px;
+	}
+`;
+
+const LogoStyled = styled.h1`
+	font-size: 2rem;
+	color: #dbc959;
+	margin-right: auto;
+	cursor: pointer;
+	a {
+		text-decoration: none;
+	}
+	@media (max-width: 1300px) {
+		margin: 0 auto 0 0;
+		/* text-align: center; */
+	}
+`;
 
 export default Header;
---
+++ frontend/components/contexts/PageContext.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default React.createContext({
+	textColorTheme       : '',
+	backgroundColorTheme : '',
+	maxWidthTheme        : '',
+	boxShadowTheme       : ''
+});
+
+/*
+export default React.createContext({
+    textColorTheme: "",
+    backgroundColorTheme: "",
+    maxWidthTheme: "",
+    boxShadowTheme: ""
+  });
+  */
