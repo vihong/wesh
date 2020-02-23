4513a662e9cf8a13e8bf45f454906924a87928b0
vihong <probamaths@gmail.com>
2020-02-23 17:56:43 +0100

    :lipstick: Add styling to Header

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 
 function Nav(props) {
 	return (
-		<>
-		    <Link href="/">
-    				<a>To HomePage</a>
-    			</Link>
-            <Link href="/sell">
-    			<a>To SellPage</a>
-    		</Link>
-		</>
+		<NavStyled>
+			<ul>
+				<li>
+					<Link href="/">
+						<a>Acheter</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/vendre">
+						<a>Vendre</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/connexion">
+						<a>Se connecter</a>
+					</Link>
+				</li>
+			</ul>
+		</NavStyled>
 	);
 }
+
+const NavStyled = styled.nav`
+	list-style: none;
+	ul li {
+		display: inline;
+		padding: 0 16px;
+		a {
+			font-size: 15px;
+			transition: all 0.3s ease 0s;
+			color: white;
+			text-transform: uppercase;
+			text-decoration: none;
+			position: relative;
+			&:hover {
+				color: gold;
+			}
+		}
+	}
+`;
 
 export default Nav;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,14 +1,55 @@
+import { useContext } from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
 function Page(props) {
 	return (
-		<div style={{ background: 'lightgreen' }}>
-			<Meta />
-			<Header />
-			{props.children}
-		</div>
+		<ThemeProvider theme={pokemonThemeValue}>
+			<PageStyled>
+				<GlobalStylePourTouteLaPage />
+				<Meta />
+				<Header />
+				<MainStyled>{props.children}</MainStyled>
+				{/* <Footer />  to be added by Toad*/}
+			</PageStyled>
+		</ThemeProvider>
 	);
 }
+
+// GlobalTheme : c'est comme si tu importer un fichier CSS avec tout le style global dedans et
+// partout où tu le positionnes dans le body, c'est bon tu as le style partout.
+const GlobalStylePourTouteLaPage = createGlobalStyle`
+    html{
+        box-sizing: border-box;
+    }
+    body{
+        margin: 0;
+        padding: 0;
+        background: pink;
+    }
+`;
+
+// LocalTheme : je n'ai le droit qu'à un seul 'theme value' par Styled Component
+const pokemonThemeValue = {
+	colorTheme           : '#393939',
+	backgroundColorTheme : '#008a83',
+	maxWidthTheme        : '1000px',
+	boxShadowTheme       : '',
+	linkColorTheme       : 'lightblue'
+};
+
+const PageStyled = styled.div`
+	background: white;
+	color: ${(props) => props.theme.colorTheme};
+`;
+
+// Ci-dessous 'theme' est un mot clé reconnu par le package 'styled-components', donc ne pas changer.
+const MainStyled = styled.main`
+	background: ${(props) => props.theme.backgroundColorTheme};
+	margin: 0 auto;
+	padding: 2rem;
+	max-width: ${(props) => props.theme.maxWidthTheme};
+`;
 
 export default Page;
