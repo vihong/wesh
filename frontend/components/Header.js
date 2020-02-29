f1204cc1e6699c8a6a78c98202f1c05445ccf8be
Tony <tony@Tonys-MacBook-Air.local>
2020-02-29 14:42:33 +0700

    :wrench: Fix load big SVG display

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -12,7 +12,7 @@ function Header(props) {
 			</Link>
 			<Menu>
 				<Nav />
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4 2h16l-3 9H4a1 1 0 1 0 0 2h13v2H4a3 3 0 0 1 0-6h.33L3 5 2 2H0V0h3a1 1 0 0 1 1 1v1zm1 18a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm10 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>
+				<svg height="30px" width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4 2h16l-3 9H4a1 1 0 1 0 0 2h13v2H4a3 3 0 0 1 0-6h.33L3 5 2 2H0V0h3a1 1 0 0 1 1 1v1zm1 18a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm10 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>
 			</Menu>
 		</HeaderStyled>
 	);
@@ -50,8 +50,6 @@ const Menu = styled.span`
 	align-items: center;
 	
 	svg {
-        height: 30px; 
-        width: 30px;
 		padding: 0 16px;
 		fill: white;
     	cursor: pointer;
