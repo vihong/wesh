0d6a4743dac96b0ec2583b2dc751a20faa31487b
Tony <tony@Tonys-MacBook-Air.local>
2020-02-29 20:00:04 +0700

    :lipstick: Add logo to the header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -7,6 +7,7 @@ function Header(props) {
 		<HeaderStyled>
 			<Link href="/">
 				<LogoStyled>
+					<img width="32px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1026px-Pok%C3%A9_Ball_icon.svg.png" alt="Pokeball icon"/>
 					<a>PokéXchange</a>
 				</LogoStyled>
 			</Link>
@@ -26,7 +27,6 @@ const HeaderStyled = styled.header`
 	height: 80px;
 	background: #07215C;
 	padding: 0 24px;
-	}
 
 	@media (max-width: 768px) {
 		height: 64px;
@@ -35,11 +35,14 @@ const HeaderStyled = styled.header`
 `;
 
 const LogoStyled = styled.h1`
-	font-size: 20px;
+	display: flex;
+	align-items: center;
+	font-size: 18px;
 	font-weight: 500;
 	color: hsl(50, 100%, 50%);
 	cursor: pointer;
 	a {
+		margin-left: 2px;
 		text-decoration: none;
 		letter-spacing: 0.03em;
 	}
