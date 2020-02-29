1d67e208f86d97b885c614a1c6f366f9f467c7fd
Tony <tony@Tonys-MacBook-Air.local>
2020-02-29 19:25:33 +0700

    :lipstick: Change the showcase image

--- frontend/components/Showcase.js
+++ frontend/components/Showcase.js
@@ -1,29 +1,21 @@
+import { useEffect } from 'react'
 import styled from 'styled-components'
 
 const Showcase = () => {
     return (
-        <div>
             <ShowcaseImage src="../static/showcase-pikachu.jpg" alt="Showcase-Pikachu"/>
-        </div>
     );
 }
 
 const ShowcaseImage = styled.img`
     object-fit: cover;
+    height: 336px;
     width:100%;
-    height: 300px;
 
-    @media (max-width: 1092px) {
-        height: 250px; 
-    }
-
-    @media (max-width: 900px) {
-        height: 200px; 
-    }
-
-    @media (max-width: 700px) {
-        object-position: 20% 50%;
+    @media (max-width: 768px) {
+        height: 300px;
     }
 `
+
 
 export default Showcase;
