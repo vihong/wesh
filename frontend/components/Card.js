ae48d0871f8eef9b161a086138138fdcda34f786
Tony <tony@Tonys-MacBook-Air.local>
2020-03-03 16:00:49 +0700

    :rocket: Style with CSS Grid

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -8,7 +8,7 @@ function Card() {
             <DescriptionCard>
                 <Pokemon>Mewtwo</Pokemon>
                 <Price>10,00€</Price>
-                <Stock><strong>2</strong> en stock</Stock>
+                <Stock><strong>2</strong> en stock !</Stock>
             </DescriptionCard>
             <BuyButton>
                 Acheter
@@ -20,6 +20,7 @@ function Card() {
 const Carddiv = styled.div`
     display: flex;
     flex-direction: column;
+    max-width: 192px;
 `
 
 const VignetteImage = styled.img`
@@ -34,20 +35,34 @@ const DescriptionCard = styled.div`
 const Pokemon = styled.h1`
     font-size: 22px;
     font-weight: 400;
+    margin-top: 6px;
 `
 
 const Price = styled.h2`
     font-size: 20px;
     font-weight: 300;
+    margin-top: 2px;
 `
 
 const Stock = styled.p`
     color: red;
     font-size: 14px;
+    margin-top: 4px;
 `
 
 const BuyButton = styled.button`
-    text-transform: uppercase
+    text-transform: uppercase;
+    display: block;
+    padding: 8px 59px;
+    margin-top: 10px;
+    background-color: #375FCC;
+    border-radius: 6px;
+    color: #FFF;
+    font-family: Poppins, Helvetica Neue;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: 0.02em;
+    cursor: pointer;
 `
 
 export default Card;
