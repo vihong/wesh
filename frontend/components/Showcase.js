78710c489053af77b081679703b0c2540d15da92
Tony <tony@Tonys-MacBook-Air.local>
2020-03-04 15:51:26 +0700

    :sparkles: Add function with hooks to update showcase img

--- frontend/components/Showcase.js
+++ frontend/components/Showcase.js
@@ -1,42 +1,32 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const Showcase = () => {
 
+    const [ImgSrc, setImgSrc] = useState("../static/showcase-pikachu.jpg")
+
+    const handleResize = event => {
+        if(event.target.innerWidth <= 1024) setImgSrc("../static/showcase-pikachu-mobile.jpg");
+        else setImgSrc("../static/showcase-pikachu.jpg")
+    }
+
     useEffect(() => {
-        console.log('test');
+        window.addEventListener('resize', handleResize);
 
     }, [])
 
     return (
             <div>
-                <ShowcaseImageDesktop src="../static/showcase-pikachu.jpg" alt="Showcase-Pikachu"/>
-                <ShowcaseImageMobile src="../static/showcase-pikachu-mobile.jpg" alt="Showcase-Pikachu"/>
+                <ShowcaseImage src={ImgSrc} alt="Showcase-Pikachu"/>
             </div>
     );
 }
 
-const ShowcaseImageDesktop = styled.img`
+const ShowcaseImage = styled.img`
     object-fit: cover;
     height: 340px;
     width:100%;
     @media (max-width: 1024px) {
-        height: 280px;
-        display: none;
-    }
-`
-
-const ShowcaseImageMobile = styled.img`
-    display: none;
-    height: 340px;
-    width:100%;
-    object-fit: cover;
-    
-    @media (max-width: 1024px) {
-        display: flex;
-    }
-
-    @media (max-width: 768px) {
         height: 280px;
     }
 `
