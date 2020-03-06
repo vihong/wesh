f2be0874e91d887ecfe676aba3dc6b5436c25525
Tony <tony@tonys-air.home>
2020-03-07 00:03:16 +0100

    :lipstick: Style the filter

--- frontend/components/SortByFilter.js
+++ frontend/components/SortByFilter.js
@@ -5,18 +5,37 @@ function SortByFilter() {
         <>
             <SortByFilterBtn>
                 Trier par
-                <svg xmlns="http://www.w3.org/2000/svg" width="5px" height="10px" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="5" viewBox="0 0 10 5"><path id="cheveron-down" d="M8.718,11.086l.625.5,5-4-1.25-1-3.75,3-3.75-3-1.25,1Z" transform="translate(-4.343 -6.586)"/></svg>
             </SortByFilterBtn>
         </>
     )
 }
 
 const SortByFilterBtn = styled.button`
-    background-color: white;
-    font-size: 14px;
-    border: 1px solid #E0E0E0;
-    border-radius: 18px;
-    color: #8D8D8D;
+    & {
+        background-color: white;
+        font-size: 14px;
+        cursor: pointer;
+        border: 1px solid #E0E0E0;
+        border-radius: 18px;
+        color: #8D8D8D;
+        outline: none;
+        display: flex;
+        align-items: center;
+        padding: 8px 10px 8px 12px;
+    }
+    &:hover {
+        background-color: #E0E0E0;
+    }
+    &:focus {
+        border: 1px solid #E0E0E0;
+        border-radius: 18px;
+    }
+
+    svg {
+        fill: #8D8D8D;
+        margin: 4px 0 0 8px;
+    }
 `
 
 export default SortByFilter;
