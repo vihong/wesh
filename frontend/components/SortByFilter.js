efec3b67136829b773470dfcd7d9bd1aaba2b947
Tony <tony@Tonys-MacBook-Air.local>
2020-03-06 15:24:29 +0100

    :tada: Start to style sort by filter

---
+++ frontend/components/SortByFilter.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+function SortByFilter() {
+    return (
+        <select name="Test" id="">
+            <option value="">JEAN</option>
+            <option value="">JEAN</option>
+            <option value="">JEAN</option>
+            <option value="">JEAN</option>
+        </select>
+    )
+}
+
+export default SortByFilter;
