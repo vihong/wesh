a1f4056d69b691ead8248d4fb08c7fd3e23b0849
Tony <tony@tonys-air.home>
2020-03-06 21:18:21 +0100

    :construction: Work on implementing the SortBy filter

--- frontend/components/SortByFilter.js
+++ frontend/components/SortByFilter.js
@@ -2,13 +2,21 @@ import styled from 'styled-components';
 
 function SortByFilter() {
     return (
-        <select name="Test" id="">
-            <option value="">JEAN</option>
-            <option value="">JEAN</option>
-            <option value="">JEAN</option>
-            <option value="">JEAN</option>
-        </select>
+        <>
+            <SortByFilterBtn>
+                Trier par
+                <svg xmlns="http://www.w3.org/2000/svg" width="5px" height="10px" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </SortByFilterBtn>
+        </>
     )
 }
+
+const SortByFilterBtn = styled.button`
+    background-color: white;
+    font-size: 14px;
+    border: 1px solid #E0E0E0;
+    border-radius: 18px;
+    color: #8D8D8D;
+`
 
 export default SortByFilter;
