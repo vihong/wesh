d17631874abd4e6033ee3a374a580ee2c69ee992
Tony KHAOV <tony.khaov@gmail.com>
2020-03-10 18:54:06 +0100

    :recycle: Refactor code

--- frontend/components/PriceFilter.js
+++ frontend/components/PriceFilter.js
@@ -1,11 +1,30 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import DropdownFilter from "./DropdownFilter";
+import { useState } from "react";
 
 const PriceFilter = () => {
-    return (
-        <div>
-            PriceFilter
-        </div>
-    )
-}
+  return (
+    <Filter>
+      <DropdownFilter>Prix</DropdownFilter>
+      <DropDownMenu>Drop</DropDownMenu>
+    </Filter>
+  );
+};
+
+const Filter = styled.div`
+  & {
+    position: relative;
+  }
+
+  &::hover > DropDownMenu {
+    display: flex;
+  }
+`;
+
+const DropDownMenu = styled.div`
+  position: absolute;
+  transform: translateX(25%);
+  border: 2px solid red;
+`;
 
 export default PriceFilter;
--- frontend/components/SortByFilter.js
+++ frontend/components/SortByFilter.js
@@ -1,38 +1,8 @@
 import styled from "styled-components";
+import DropdownFilter from "./DropdownFilter";
 
 function SortByFilter() {
-  return (
-    <>
-      <SortByFilterBtn>Trier par</SortByFilterBtn>
-    </>
-  );
+  return <DropdownFilter>Trier par</DropdownFilter>;
 }
-
-const SortByFilterBtn = styled.button`
-  & {
-    background-color: white;
-    font-size: 14px;
-    cursor: pointer;
-    border: 1px solid #e0e0e0;
-    border-radius: 18px;
-    color: #8d8d8d;
-    outline: none;
-    display: flex;
-    align-items: center;
-    padding: 8px 10px 8px 12px;
-  }
-  &:hover {
-    background-color: #e0e0e0;
-  }
-  &:focus  {
-    border: 1px solid #e0e0e0;
-    border-radius: 18px;
-  }
-
-  svg {
-    fill: #8d8d8d;
-    margin: 4px 0 0 8px;
-  }
-`;
 
 export default SortByFilter;
