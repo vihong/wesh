7eb25cb29d3515e36560107acd395372d9be2923
Tony KHAOV <tony.khaov@gmail.com>
2020-03-10 18:55:41 +0100

    :bug: Fix a bug

--- frontend/components/PriceFilter.js
+++ frontend/components/PriceFilter.js
@@ -4,27 +4,10 @@ import { useState } from "react";
 
 const PriceFilter = () => {
   return (
-    <Filter>
+    <>
       <DropdownFilter>Prix</DropdownFilter>
-      <DropDownMenu>Drop</DropDownMenu>
-    </Filter>
+    </>
   );
 };
-
-const Filter = styled.div`
-  & {
-    position: relative;
-  }
-
-  &::hover > DropDownMenu {
-    display: flex;
-  }
-`;
-
-const DropDownMenu = styled.div`
-  position: absolute;
-  transform: translateX(25%);
-  border: 2px solid red;
-`;
 
 export default PriceFilter;
